a31802f77ca0f12cf70d437dde067bde9dee7899
mahalyka <fpangiosty@gmail.com>
2025-04-10 23:55:49 +0700

    add env and constant

---
+++ src/constant/global-constant.js
@@ -0,0 +1,10 @@
+module.exports = {
+    HUBSPOT_BASE_URL: process.env.HUBSPOT_BASE_URL,
+    CUSTOM_OBJECTS: {
+        ENQUIRIES: '2-43175220',
+        CONTACT: 'contacts'
+    },
+    SYSTEM: {
+        HS_TOKEN: process.env.HS_TOKEN
+    }
+  };
